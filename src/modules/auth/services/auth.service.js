89f6eac17db6265f49221aae670ff4a8c5f1a0cc
Eugen Istoc <eugenistoc@gmail.com>
2015-09-03 13:42:28 -0400

    Basic boilerplate for auth service

--- src/modules/auth/services/auth.service.js
+++ src/modules/auth/services/auth.service.js
@@ -1,5 +1,36 @@
 'use strict';
 
-angular.module('module.auth').service('AuthService', function() {
+angular.module('module.auth').service('AuthService', function(store, PACKAGE) {
+  var isAuthenticated = false;
+  var role = '';
+  var session = null;
 
+  return {
+    login: function() {
+      // Implement custom login
+      // store.set(package.name + '.session', 'CUSTOM SESSION');
+    },
+    logout: function() {
+      // Implement custom logout
+      // store.remove(package.name + '.session');
+    },
+    session: function() {
+      return session;
+    },
+    isAuthorized: function(authorizedRoles) {
+      if (!angular.isArray(authorizedRoles)) {
+        authorizedRoles = [authorizedRoles];
+      }
+
+      // If authorizedRoles role has * allow passthrough
+      if (authorizedRoles.indexOf('*') !== -1) {
+        return true;
+      } else {
+        return (authorizedRoles.indexOf(role) !== -1);
+      }
+    },
+    isAuthenticated: function() {
+      return isAuthenticated;
+    }
+  }
 })
