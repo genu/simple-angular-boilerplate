e06abc378e8fbcf24a477990591b62394d2ea9a8
Eugen Istoc <eugenistoc@gmail.com>
2015-10-16 13:46:59 -0400

    Auto select server port

--- grunt/connect.js
+++ grunt/connect.js
@@ -1,21 +1,21 @@
-module.exports = function () {
-    return {
-        options: {
-            port: 9000,
-            hostname: 'localhost',
-            livereload: 35729
-        },
-        livereload: {
-            options: {
-                open: true,
-                middleware: function (connect) {
-                    return [
-                        connect.static('.tmp'),
-                        connect().use('/bower_components', connect.static('./bower_components')),
-                        connect.static('src')
-                    ]
-                }
-            }
+module.exports = function() {
+  return {
+    options: {
+      hostname: 'localhost',
+      livereload: 35729
+      useAvailablePort: true,
+    },
+    livereload: {
+      options: {
+        open: true,
+        middleware: function(connect) {
+          return [
+            connect.static('.tmp'),
+            connect().use('/bower_components', connect.static('./bower_components')),
+            connect.static('src')
+          ]
         }
+      }
     }
+  }
 };
