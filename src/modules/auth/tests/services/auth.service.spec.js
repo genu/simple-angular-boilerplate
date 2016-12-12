4bc68b04fc53d80fb2d59c2d382609c2bad26a5c
Eugen Istoc <eugenistoc@gmail.com>
2016-12-12 10:56:47 -0500

    Latest

---
+++ src/modules/auth/tests/services/auth.service.spec.js
@@ -0,0 +1,26 @@
+'use strict';
+
+describe('Testing module.auth:AuthService', function() {
+
+  beforeEach(module('module.auth'));
+
+  it('should login a user', function() {
+    expect('test').toBe('test');
+  })
+
+  it('should logout a user', function() {
+
+  })
+
+  it('should get a user session', function() {
+
+  })
+
+  it('should check if a user is authorized', function() {
+
+  })
+
+  it('should check if a user is authenticated', function() {
+
+  })
+})
